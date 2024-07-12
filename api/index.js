41d183b78bbb348a54a1cd2050cd3459ab8d0162
Shashikant3006 <shashisp8757@gmail.com>
2024-07-12 14:09:13 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import dotenv from "dotenv"
+import cors from "cors"
+import "./db/database.js"
+import UserRouter from "./routes/user.js"
+import BlogRouter from "./routes/blog.js"
+
+dotenv.config()
+
+const port = process.env.PORT;
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.setHeader("Access-Control-Allow-Credentials","true");
+    res.send("API is running...");
+});
+
+app.use("/api/user",UserRouter) 
+app.use("/api/blog",BlogRouter) 
+
+
+app.use((req,res,next)=>{
+    const error = new Error(`Not Found -${req.originalUrl}`);
+    res.status(400);
+    next(error);
+})
+app.use((err,req,res,next)=>{
+    const statusCode = res.statusCode === 200? 500: res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message:err.message,
+        stack:process.env.NODE_ENV === "prdouction"? null :err.stack,
+    });
+    next();
+})
+
+
+const PORT = process.env.PORT || 7000;
+
+app.listen(port,()=>{
+    console.log("app is running on port",port);
+})
